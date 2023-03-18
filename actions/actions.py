8e4e0d861dc63b211f775e63e7322800881cee12
Aimen Altaiyeb <aaltaiyeb@godaddy.com>
2023-03-18 12:31:47 -0700

    New Initial commit

--- actions/actions.py
+++ actions/actions.py
@@ -6,7 +6,7 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
-
+#
 # from typing import Any, Text, Dict, List
 #
 # from rasa_sdk import Action, Tracker
@@ -25,3 +25,23 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionExtractName(Action):
+    def name(self) -> Text:
+        return "action_extract_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        name = None
+        for entity in tracker.latest_message['entities']:
+            if entity['entity'] == 'name':
+                name = entity['value']
+                break
+        return [SlotSet('name', name)]
